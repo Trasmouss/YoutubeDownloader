04fb921de1b352dbe4afd25559bbf103ef349daf
Cihan Demir <cihandemir7@hotmail.com>
2022-06-05 15:03:23 +0300

    Create Tkinter Window

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+from tkinter import *
+
 # This is a sample Python script.
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+
+class Window(object):
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.resizable(width=FALSE, height=FALSE)
+        self.window.geometry("600x400+300+300")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("TEST")
+    w = Window()
+    mainloop()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
