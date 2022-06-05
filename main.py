72792ade597392839e4407c07557b8cef28ae602
Cihan Demir <cihandemir7@hotmail.com>
2022-06-05 16:53:58 +0300

    add pytube and get video list functions.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from pytube import YouTube
+import threading
 
 # This is a sample Python script.
 # Press Shift+F10 to execute it or replace it with your code.
@@ -12,6 +14,9 @@ class Window(object):
         self.window.resizable(width=FALSE, height=FALSE)
         self.window.geometry("600x400+300+300")
         self.window.title("Youtube Downloader")
+
+        self.yt = ""
+        self.video = ""
 
         self.alert = Label(text="Wellcome to Youtube Downloader. Pleace insert Youtube Link.",
                            fg="red",
@@ -44,11 +49,43 @@ class Window(object):
     def items_selected(self, event):
         pass
 
+    def set_videos(self):
+        self.alert.config(text="Please Wait")
+        self.yt = YouTube(self.linkentry.get(), on_progress_callback=self.on_progress,
+                          on_complete_callback=self.on_complete)
+        self.alert.config(text=self.yt.title)
+        self.video = self.yt.streams
+        self.setlist()
+
     def get_videos(self):
-        pass
+        if self.linkentry.get() is not "" and self.pathentry.get() is not "":
+            self.get_btn["state"] = DISABLED
+            threading.Thread(target=self.set_videos).start()
+        else:
+            self.alert.config(text="Please Set All Fields.")
+
+    def setlist(self):
+        self.video = self.video.order_by('resolution')
+        for stream in self.video:
+            line = str(stream.itag) + \
+                   " RES : " + str(stream.resolution) + \
+                   " FPS : " + str(stream.fps) + \
+                   " AU : " + str(stream.audio_codec) + \
+                   " VD : " + str(stream.video_codec) + \
+                   " TYPE : " + str(stream.mime_type)
+            self.list.insert(END, line)
+            # print(stream)
+            self.get_btn["state"] = NORMAL
 
     def download(self):
         pass
+
+    def on_progress(self):
+        pass
+
+    def on_complete(self):
+        pass
+
 
 
 
